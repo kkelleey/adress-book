ac23ff90a5aca53d191b0db94bc674060e0170c5
Karen Kelley <karenkelley11@gmail.com>
2015-10-10 15:01:09 -0400

    adding edit functionality

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('contacts', { path: '/' }, function() {
     this.route('contact', { path: '/:contact_id' }, function() {});
     this.route('new', { path: '/new' }, function() {});
+    this.route('edit', { path: '/:contact_id/edit' }, function() {});
   });
 });
 
